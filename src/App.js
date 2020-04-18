b16e40998a671a7a38c73103344f55ec7f81a40d
zainabal-haidary <57359534+zainabal-haidary@users.noreply.github.com>
2020-04-18 18:35:05 +0100

    local push notification

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { YellowBox } from 'react-native';
+import { YellowBox, Alert } from 'react-native';
 import AppNavigator from './AppNavigator';
-
+// import NotificationService from './NotificationService';
 export default class App extends React.PureComponent {
     constructor(props) {
         YellowBox.ignoreWarnings(['Warning:']);//ignore yellow box messages that starts with "Wraning:"
         console.disableYellowBox = true;
+        // this.notification = new NotificationService(this.onNotification);
         super();
     }
 
+    // //Gets called when the notification comes in
+    // onNotification = (notif) => {
+    //     Alert.alert(notif.title, notif.message);
+    // }
+
+    // //Permissions to use notifications
+    // handlePerm(perms) {
+    //     Alert.alert("Permissions", JSON.stringify(perms));
+    // }
+
+    // componentDidMount =() =>{
+    //     this.notification.localNotification();
+    //     this.notification.scheduleNotification();
+    // }
     render() {
-        return (
-            // <Root>
-                <AppNavigator />
-            // </Root>
-        );
+        return (<AppNavigator />);
     }
 }
